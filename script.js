4a533daf37b5688931514919acd7df3b790610f0
Yashvi-Khandelwal <khandelwalyashvi427@gmail.com>
2025-12-18 17:51:07 +0530

    design updated

--- script.js
+++ script.js
@@ -1,71 +1,93 @@
-const advantages = [
-  {
-    icon: "assets/wireless-network.png",
-    title: "Continuous Vitals Monitoring",
-    desc: "Measures HR, RR, BP, and SpO₂ with clinical-grade accuracy."
-  },
-  {
-    icon: "assets/telemetry.png",
-    title: "Automated Workflow",
-    desc: "Integrates with EMRs like PointClickCare and MatrixCare to log vitals and notifications."
-  },
-  {
-    icon: "assets/hospital-bed.png",
-    title: "Personalized Patient Monitoring",
-    desc: "Allows tailored thresholds and notifications based on each patient’s condition."
-  },
-  {
-    icon: "images/security.svg",
-    title: "Enterprise-Grade Data Security",
-    desc: "Ensures HIPAA, SOC2-Type I, and ISO 27001-compliant health data protection."
-  },
-  {
-    icon: "images/dashboard.svg",
-    title: "Central Monitoring Platform",
-    desc: "Offers one dashboard for vitals, notifications, trends, and fall prevention tracking."
-  },
-  {
-    icon: "images/reimbursement.svg",
-    title: "Reimbursement Optimization",
-    desc: "Supports CPT billing codes under CMS for RPM, CCM, and related services."
-  }
-];
 
-const container = document.getElementById("advantageCards");
+  const advantages = [
+    {
+      icon: "./assets/wireless-network.png",
+      title: "Continuous Vitals Monitoring",
+      desc: "Measures HR, RR, BP, and SpO₂ with clinical-grade accuracy."
+    },
+    {
+      icon: "./assets/telemetry.png",
+      title: "Automated Workflow",
+      desc: "Integrates with EMRs like PointClickCare and MatrixCare to log vitals and notifications."
+    },
+    {
+      icon: "./assets/hospital-bed.png",
+      title: "Personalized Patient Monitoring",
+      desc: "Allows tailored thresholds and notifications based on each patient’s condition."
+    },
+    {
+      icon: "./assets/alert.png",
+      title: "Enterprise-Grade Data Security",
+      desc: "Ensures HIPAA, SOC2-Type I, and ISO 27001-compliant health data protection."
+    },
+    {
+      icon: "./assets/medical-report.png",
+      title: "Central Monitoring Platform",
+      desc: "Offers one dashboard for vitals, notifications, trends, and fall prevention tracking."
+    },
+    {
+      icon: "./assets/monitor.png",
+      title: "Reimbursement Optimization",
+      desc: "Supports CPT billing codes under CMS for RPM, CCM, and related services."
+    }
+  ];
 
-advantages.forEach(item => {
-  const card = document.createElement("div");
-  card.className = "card";
+  const container = document.getElementById("advantageCards");
 
-  card.innerHTML = `
-    <img src="${item.icon}" alt="${item.title}">
-    <h3>${item.title}</h3>
-    <p>${item.desc}</p>
-  `;
+  advantages.forEach(item => {
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-  container.appendChild(card);
-});
+    card.innerHTML = `
+      <img src="${item.icon}" alt="${item.title}">
+      <h3>${item.title}</h3>
+      <p>${item.desc}</p>
+    `;
 
-const track = document.getElementById("sliderTrack");
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
+    container.appendChild(card);
+  });
 
-const cardWidth = 384; 
-let currentPosition = 0;
+  const socialIconsData = [
+    {
+      name: "LinkedIn",
+      link: "#",
+      img: "assets/linkedin.png"
+    },
+    {
+      name: "Facebook",
+      link: "#",
+      img: "assets/facebook.png"
+    },
+    {
+      name: "Twitter",
+      link: "#",
+      img: "assets/x.png"
+    },
+    {
+      name: "YouTube",
+      link: "#",
+      img: "assets/youtube.png"
+    },
+    {
+      name: "Instagram",
+      link: "#",
+      img: "assets/insta.png"
+    }
+  ];
 
-const maxScroll =
-  track.scrollWidth - document.querySelector(".slider-container").offsetWidth;
+  const socialIconsContainer = document.getElementById("socialIcons");
 
-prevBtn.addEventListener("click", () => {
-  currentPosition -= cardWidth;
-  if (currentPosition < 0) currentPosition = 0;
-  track.style.transform = `translateX(-${currentPosition}px)`;
-});
+  socialIconsData.forEach(icon => {
+    const aTag = document.createElement("a");
+    aTag.href = icon.link;
+    aTag.target = "_blank";
 
-nextBtn.addEventListener("click", () => {
-  currentPosition += cardWidth;
-  if (currentPosition > maxScroll) currentPosition = maxScroll;
-  track.style.transform = `translateX(-${currentPosition}px)`;
-});
+    const imgTag = document.createElement("img");
+    imgTag.src = icon.img;
+    imgTag.alt = icon.name;
+
+    aTag.appendChild(imgTag);
+    socialIconsContainer.appendChild(aTag);
+  });
 
 
